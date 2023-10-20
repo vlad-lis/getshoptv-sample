d8576bbd0f2bb3c4b38cd1d30882920d6bf6fb0b
vladislav lis <lis.vladislav@gmail.com>
2023-10-20 10:54:58 +0400

    feat: add main page

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,15 +1,30 @@
 import { ReactElement } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Banner.module.scss';
-import banner from '../../static/banner.json';
 
 const Banner = (): ReactElement => {
+  const navigate = useNavigate();
+
+  const handleButtonClick = (): void => {
+    navigate('/promo');
+  };
+
   return (
     <header className={styles.banner}>
-      <h1 className={styles.banner__title}>{banner.title}</h1>
+      <h1 className={styles.banner__title}>
+        ИСПОЛНИТЕ МЕЧТУ ВАШЕГО МАЛЫША! <br />
+        ПОДАРИТЕ ЕМУ СОБАКУ!
+      </h1>
       <div className={styles.banner__qr} />
-      <p className={styles.banner__subtitle}>{banner.subtitle}</p>
-      <button className={styles.banner__btn} type='button'>
-        {banner.button}
+      <p className={styles.banner__subtitle}>
+        Сканируйте QR-код или нажмите ОК
+      </p>
+      <button
+        className={styles.banner__btn}
+        type='button'
+        onClick={handleButtonClick}
+      >
+        ОК
       </button>
     </header>
   );
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,37 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import Banner from '../../components/Banner/Banner';
+import styles from './MainPage.module.scss';
+import doggieSource from '../../videos/doggie.mp4';
 
 const MainPage = (): ReactElement => {
+  // banner visibility
+  const [isBannerVisible, setIsBannerVisible] = useState(false);
+  const bannerClass = isBannerVisible
+    ? styles.main__banner
+    : styles.main__banner_hidden;
+
+  // display banner after 5 sec
+  useEffect(() => {
+    const delay = 5000;
+    const timer = setTimeout(() => {
+      setIsBannerVisible(true);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  });
+
   return (
-    <div>
-      <Banner />
-    </div>
+    <main className={styles.main}>
+      <video className={styles.main__video} autoPlay muted loop>
+        <source src={doggieSource} type='video/mp4' />
+        oh no
+      </video>
+      <div className={bannerClass}>
+        <Banner />
+      </div>
+    </main>
   );
 };
 
